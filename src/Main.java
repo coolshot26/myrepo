2febcc2544429c9a5634606f27b13cf0cf89ad65
510372 <510372@DTV-D95293.LA.FRD.DIRECTV.com>
2016-05-06 11:38:35 -0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String hello = "Hello World!!";
+		String hello = "Hello World!! Hope you don't rain.";
 		System.out.println("Message: " + hello);
 	}
 
